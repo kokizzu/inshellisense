86bdd7b23d1c57978a01e58d45f289922914f1f0
Chapman Pendery <cpendery@vt.edu>
2026-03-21 19:48:23 -0700

    build: bump formula version
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- Formula/inshellisense.rb
+++ Formula/inshellisense.rb
@@ -1,8 +1,8 @@
 class Inshellisense < Formula
   desc "IDE style command line auto complete"
   homepage "https://github.com/microsoft/inshellisense"
-  url "https://registry.npmjs.org/@microsoft/inshellisense/-/inshellisense-0.0.1-rc.32.tgz"
-  sha256 "1240ac910d30dfbef39d4813d1e347dfd538a315fb0c080fc41d747455c0e837"
+  url "https://registry.npmjs.org/@microsoft/inshellisense/-/inshellisense-0.0.1.tgz"
+  sha256 "2d1ce2ddfc7340f07990b34b14bc913d3ccf3f50c43e0a9ebee932ce05919f9b"
   license "MIT"
 
   depends_on "node"
